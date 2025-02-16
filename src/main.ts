20c032e32b5e2abf3feac1fc57e68a182b882580
Erick Melgar <kcire89@gmail.com>
2025-02-16 11:16:26 -0500

    Add transport NATS

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+
+  const logguer = new Logger('OrderMS-MAIN');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+    }
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  await app.listen();
+  logguer.log(`Microservices Orders running in ${envs.port}`)
+}
+bootstrap();
